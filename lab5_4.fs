302c20dc52070c541b2344d3a48a82e2f08ca293
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-03-29 10:15:02 +0300

    task_4

---
+++ lab5_4.fs
@@ -0,0 +1,13 @@
+let rec sumOfDigits n =
+     if n = 0 then
+         0 
+     else
+         (n % 10) + sumOfDigits (n / 10) 
+
+
+printfn "Введите число:"
+let number = System.Console.ReadLine() |> int 
+ 
+let sum = sumOfDigits number 
+printfn "Сумма цифр числа %d равна %d" number sum 
+ 
